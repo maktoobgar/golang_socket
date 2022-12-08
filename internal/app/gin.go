84876e0ba1f5d3d791dd3486e2f6efdf78998ff9
Mahmood Heidari <mahmoodh1378@gmail.com>
2022-12-08 17:32:14 +0330

    🔧 refactor: adding cors

--- internal/app/gin.go
+++ internal/app/gin.go
@@ -3,16 +3,32 @@ package app
 import (
 	"fmt"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/maktoobgar/golang_socket/internal/configs"
 	"github.com/maktoobgar/golang_socket/internal/routers"
 )
+
+func configCors(r *gin.Engine) {
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowWebSockets = true
+	if configs.CFG.Debug {
+		corsConfig.AllowOriginFunc = func(origin string) bool {
+			return true
+		}
+	} else {
+		corsConfig.AllowOrigins = configs.CFG.AllowOrigins
+	}
+
+	r.Use(cors.New(corsConfig))
+}
 
 func Gin() {
 	if !configs.CFG.Debug {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r := gin.Default()
+	configCors(r)
 
 	routers.ApplyAllRouters(r)
 
